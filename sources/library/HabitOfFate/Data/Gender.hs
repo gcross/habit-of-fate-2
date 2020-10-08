8f881186cca865b1886a1f46a1ecad9679fee109
Gregory Crosswhite <gcrosswhite@gmail.com>
2020-10-08 10:05:19 -0400

    Dropped now-unused JSON support.

--- sources/library/HabitOfFate/Data/Gender.hs
+++ sources/library/HabitOfFate/Data/Gender.hs
@@ -23,39 +23,11 @@
 
 module HabitOfFate.Data.Gender (Gender(..),Gendered(..)) where
 
-import Data.Aeson (FromJSON(..), ToJSON(..), Value(..), (.:), object, withObject, withText)
-import Data.String.Interpolate (i)
 import Data.Text (Text)
 
-import HabitOfFate.JSON
-
 data Gender = Male | Female | Neuter deriving (Enum,Eq,Ord,Read,Show)
-
-instance ToJSON Gender where
-  toJSON Male = String "male"
-  toJSON Female = String "female"
-  toJSON Neuter = String "neuter"
-
-instance FromJSON Gender where
-  parseJSON = withText "expected text for the gender" $ \case
-    "male" → pure Male
-    "female" → pure Female
-    "neuter" → pure Neuter
-    wrong → fail [i|gender must be "male", "female", or "neuter", not "#{wrong}"|]
 
 data Gendered = Gendered
   { name ∷ Text
   , gender ∷ Gender
   } deriving (Eq,Ord,Read,Show)
-
-instance ToJSON Gendered where
-  toJSON Gendered{..} = object
-    [ "name" .== toJSON name
-    , "gender" .== toJSON gender
-    ]
-
-instance FromJSON Gendered where
-  parseJSON = withObject "gendered entity must have object shape" $ \o →
-    Gendered
-      <$> (o .: "name" >>= parseJSON)
-      <*> (o .: "gender" >>= parseJSON)
